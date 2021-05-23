df0434421ff7768204818a85931e6dcba36145e2
Alessio Cutaia <s262400@studenti.polito.it>
2021-05-23 09:48:24 +0200

    Soluzione

--- src/main/java/it/polito/tdp/poweroutages/FXMLController.java
+++ src/main/java/it/polito/tdp/poweroutages/FXMLController.java
@@ -5,9 +5,14 @@
 package it.polito.tdp.poweroutages;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import com.sun.javafx.scene.paint.GradientUtils.Point;
+
 import it.polito.tdp.poweroutages.model.Model;
 import it.polito.tdp.poweroutages.model.Nerc;
+import it.polito.tdp.poweroutages.model.PowerOutages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -38,7 +43,46 @@ public class FXMLController {
     
     @FXML
     void doRun(ActionEvent event) {
+    	
     	txtResult.clear();
+    	
+    	try {
+    		
+    		Nerc selectedNerc = cmbNerc.getSelectionModel().getSelectedItem();
+    		if(selectedNerc == null) {
+    			txtResult.setText("Seleziona un Nerc (Area identifier)");
+    			return;
+    		}
+    		
+    		int maxY = Integer.parseInt(txtYears.getText());
+			if (maxY <= 0) {
+				txtResult.setText("Seleziona un numero di anni maggiore di 0");
+				return;
+			}
+    		
+    		int maxH = Integer.parseInt(txtHours.getText());
+    		if(maxH <= 0) {
+    			txtResult.setText("Seleziona un numero di ore maggiore di 0");
+    			return;
+    		}
+    		
+    		txtResult.setText(
+					String.format("Computing the worst case analysis... for %d hours and %d years", maxH, maxY));
+			List<PowerOutages> worstCase = model.getWorstCase(maxY, maxH, selectedNerc);
+
+			txtResult.clear();
+			txtResult.appendText("Tot people affected: " + model.sumAffectedPeople(worstCase) + "\n");
+			txtResult.appendText("Tot hours of outage: " + model.sumOutageHours(worstCase) + "\n");
+    		
+    		for (PowerOutages ee : worstCase) {
+				txtResult.appendText(String.format("%d %s %s %d %d", ee.getYear(), ee.getOutageStart(),
+						ee.getOutageEnd(), ee.getOutageDuration(), ee.getAffectedPeople()));
+				txtResult.appendText("\n");
+			}
+    		
+    	} catch (NumberFormatException e) {
+			txtResult.setText("Insert a valid number of years and of hours");
+		}
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -54,5 +98,7 @@ public class FXMLController {
     
     public void setModel(Model model) {
     	this.model = model;
+    	List<Nerc> nercList = model.getNercList();
+    	cmbNerc.getItems().addAll(nercList);
     }
 }
